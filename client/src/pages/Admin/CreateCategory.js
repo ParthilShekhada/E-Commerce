1adf614b222ee3119c7d776769ecd1cd6b4eb33b
Parthil Shekhada <shekhadaparthil@gmail.com>
2023-06-02 10:12:01 +0530

    Minor fixes

--- client/src/pages/Admin/CreateCategory.js
+++ client/src/pages/Admin/CreateCategory.js
@@ -3,10 +3,30 @@ import Layouts from '../../components/Layouts/Layouts'
 import AdminMenu from '../../components/Layouts/AdminMenu'
 import { toast } from 'react-hot-toast'
 import axios from 'axios'
+import CategoryForm from '../../components/Form/CategoryForm'
 
 
 const CreateCategory = () => {
     const [categories, setCategories] = useState([]);
+    const [name, setName] = useState("")
+    const [visible, setVisible] = useState(false);
+    const [selected, setSelected] = useState(null);
+    const [updatedName, setUpdatedName] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const {data} = await axios.post(`${process.env.REACT_APP_API}/category/create-category`, { name })
+
+            if (data.error == false) {
+                toast.success(`${name} is created`)
+                getAllCategory()
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error("Something Went Wrong")
+        }
+    }
 
     const getAllCategory = async () => {
         try {
@@ -14,8 +34,8 @@ const CreateCategory = () => {
 
             if (data.error == false) {
                 setCategories(data.category)
+
             }
-            console.log(categories)
         } catch (error) {
             console.log(error)
             toast("Something went wrong when getting category")
@@ -29,7 +49,43 @@ const CreateCategory = () => {
 
     }, [])
 
+    const handleUpdate = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await axios.put(
+                `${process.env.REACT_APP_API}/category/update-category/${selected._id}`,
+                { name: updatedName }
+            );
+            if (data.error==false) {
+                toast.success(`${updatedName} is updated`);
+                setSelected(null);
+                setUpdatedName("");
+                setVisible(false);
+                getAllCategory();
+            } else {
+                toast.error("Something went Wrong");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    //delete category
+    const handleDelete = async (pId) => {
+        try {
+            const { data } = await axios.delete(
+                `${process.env.REACT_APP_API}/category/delete-category/${pId}`
+            );
+            if (data.error==false) {
+                toast.success(`category is deleted`);
 
+                getAllCategory();
+            } else {
+                toast.error(data.message);
+            }
+        } catch (error) {
+            toast.error("Somtihing went wrong");
+        }
+    };
     return (
         <Layouts title={'Dashboard-Create Category'}>
             <div className="container-fluid m-3 p-3">
@@ -39,6 +95,9 @@ const CreateCategory = () => {
                     </div>
                     <div className="col-md-9">
                         <h1>Manage Category</h1>
+                        <div className="p-3">
+                            <CategoryForm handleSubmit={handleSubmit} value={name} setValue={setName} />
+                        </div>
                         <div>
                             <table className="table">
                                 <thead>
@@ -48,11 +107,17 @@ const CreateCategory = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        {categories.map(c=>{
-                                            <td key={c._id}>c.name</td>
-                                        })}
-                                    </tr>
+
+                                    {categories.map((c) => (
+                                        <>
+                                            <tr>
+                                                <td key={c._id}>{c.name}</td>
+                                                <td><button key={c._id} className="btn btn-primary ms-2">Edit</button>
+                                                    <button key={c._id} className="btn btn-danger ms-2" onClick={() => { handleDelete(c._id); }}> Delete </button>
+                                                </td>
+                                            </tr>
+                                        </>
+                                    ))}
                                 </tbody>
                             </table>
 
